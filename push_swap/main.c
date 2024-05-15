67f647de575b198910bd318729e14b9b69b7e29c
Teghjyot Singh <teghjyot@icloud.com>
2024-05-15 15:33:53 +0930

    Initial Commit

---
+++ push_swap/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  push_swap
+//
+//  Created by Teghjyot Singh on 15/5/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
